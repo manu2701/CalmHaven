86daf3a29981ff8375bb47f2c5ca3cf5b0f01676
Mohith Gowda R <mohithgowdait@gmail.com>
2025-01-02 20:38:45 +0530

    Update index.js

--- Backend/config/index.js
+++ Backend/config/index.js
@@ -4,7 +4,7 @@ module.exports = {
     PORT: process.env.PORT || 8080,
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/calmhaven',
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'your-secret-key',
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY || 'AIzaSyAfJNEv2bB5r0cKbnION3nPlqu65ka_pWA',
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY ,
     // Rate limiting
     RATE_LIMIT_WINDOW: 60 * 60 * 1000, // 1 hour in milliseconds
     RATE_LIMIT_MAX_REQUESTS: 100, // Maximum requests per window
